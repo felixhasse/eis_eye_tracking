6a0c5ae8b760b86d3f93d974874c584e01f82e94
Felix Hasse <fh@becklyn.com>
2023-12-13 13:26:33 +0100

    Various changes

--- run_hubii_pipeline.py
+++ run_hubii_pipeline.py
@@ -1,9 +1,28 @@
 import glob
 import json
 import os
+from io import StringIO
+import time
+
+import pandas as pd
 import requests
 
-from split_eyetracking_file import split_eyetracking_file
+
+def split_eyetracking_file():
+    # Read in the data
+    df = pd.read_csv('data/EyeTrackingData.csv')
+
+    # Split by sessionId
+    session_ids = df['sessionId'].unique()
+
+    for session_id in session_ids:
+        # Get the data for this session
+        session_df = df[df['sessionId'] == session_id]
+
+        # Save the data to a file with the session id as the name
+        if not os.path.exists('data/eye_tracking'):
+            os.mkdir('data/eye_tracking')
+        session_df.to_csv(f'data/eye_tracking/{session_id}.csv', index=False, )
 
 
 def send_request(file_path):
@@ -28,20 +47,19 @@ def send_request(file_path):
 
     # Send the request to the server
     response = requests.post(request_url, data=request_body, files=files)
-
     return response.json()["task"]
 
 
 def get_fixation_data(for_task_id):
-    request_url = f'https://eyetracking-01-hubii.k8s.iism.kit.edu/eyetracking/fixation'
+    request_url = f'https://eyetracking-01-hubii.k8s.iism.kit.edu/eyetracking/result/fixation'
     response = requests.get(request_url, params={'task_id': for_task_id})
-    return response.json()
+    return response.text
 
 
 def get_saccade_data(for_task_id):
-    request_url = f'https://eyetracking-01-hubii.k8s.iism.kit.edu/eyetracking/saccades'
+    request_url = f'https://eyetracking-01-hubii.k8s.iism.kit.edu/eyetracking/result/saccades'
     response = requests.get(request_url, params={'task_id': for_task_id})
-    return response.json()
+    return response.text
 
 
 if __name__ == '__main__':
@@ -53,7 +71,7 @@ if __name__ == '__main__':
     # Get all csv files
     print("Loading files")
     eyetracking_files = glob.glob('data/eye_tracking/*.csv')
-    eyetracking_filenames = [os.path.basename(file) for file in eyetracking_files]
+    eyetracking_filenames = [os.path.basename(file).split(".")[0] for file in eyetracking_files]
     task_ids = []
 
     for index, eyetracking_file in enumerate(eyetracking_files):
@@ -61,13 +79,34 @@ if __name__ == '__main__':
         task_id = send_request(eyetracking_file)
         task_ids.append(task_id)
 
+    all_tasks = []
+    time.sleep(100)
     for index, task_id in enumerate(task_ids):
         print(f"Getting data for task {index + 1}/{len(task_ids)}")
+
+        fixations_data = StringIO(get_fixation_data(task_id))
+        saccades_data = StringIO(get_saccade_data(task_id))
+
+        fixations_df = pd.read_csv(fixations_data)
+        saccades_df = pd.read_csv(saccades_data)
+
         task_data = {
-            'fixations': get_fixation_data(task_id),
-            'saccades': get_saccade_data(task_id),
+            'fixations': fixations_df,
+            'saccades': saccades_df,
         }
+
         if not os.path.exists('data/eye_tracking/results'):
             os.mkdir('data/eye_tracking/results')
-        with open(f'data/eye_tracking/results/{eyetracking_filenames[index]}.json', 'w') as file:
-            json.dump(task_data, file)
+
+        fixations_df.to_csv(f'data/eye_tracking/results/{eyetracking_filenames[index]}_fixations.csv', index=False)
+        saccades_df.to_csv(f'data/eye_tracking/results/{eyetracking_filenames[index]}_saccades.csv', index=False)
+
+        all_tasks.append(task_data)
+
+    all_fixations_df = pd.concat([task['fixations'] for task in all_tasks])
+    all_saccades_df = pd.concat([task['saccades'] for task in all_tasks])
+
+    all_fixations_df.to_csv('data/eye_tracking/results/all_fixations.csv', index=False)
+    all_saccades_df.to_csv('data/eye_tracking/results/all_saccades.csv', index=False)
+
+    print("Done")
